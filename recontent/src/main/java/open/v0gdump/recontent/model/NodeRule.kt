ebddf874714e319603157ab78225fcaf5bca0f2c
Klachkov Valery <0gdump.kv@gmail.com>
2020-08-01 02:13:03 +1000

    [lib] Nullable matchCallback in NodeRule

--- recontent/src/main/java/open/v0gdump/recontent/model/NodeRule.kt
+++ recontent/src/main/java/open/v0gdump/recontent/model/NodeRule.kt
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element
 
 data class NodeRule(
     val selector: String,
-    val matchCallback: (element: Element, tag: String?) -> Unit,
+    val matchCallback: ((element: Element, tag: String?) -> Unit)? = null,
     val treeParsedCallback: ((element: Element, tag: String?) -> Unit)? = null,
     val sectionRule: SectionRule? = null,
     val tag: String? = null
